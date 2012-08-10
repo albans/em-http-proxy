1029f1805aaa788aa4cdbdc0cf982a077d2b25b9
Alban Scholer <alban.scholer@gmail.com>
2012-08-11 00:13:29 +0200

    first revision

---
+++ em-proxy.rb
@@ -0,0 +1,45 @@
+﻿require "rubygems"
+require "eventmachine"
+
+module ProxyConnection
+  def initialize(client, request)
+    @client, @request = client, request
+  end
+
+  def post_init
+    puts "#{@request}"
+    EM::enable_proxy(self, @client)
+  end
+
+  def connection_completed
+    send_data @request
+  end
+
+  def proxy_target_unbound
+    close_connection
+    puts "Connection closed."
+  end
+
+  def unbind
+    @client.close_connection_after_writing
+  end
+end
+
+module ProxyServer
+  def receive_data(data)
+    (@buf ||= "") << data
+    if @buf =~ /\r\n\r\n/ # all http headers received
+      
+      if @buf =~ /Host: ([\w\.]*)(:(\d*))?/
+        host=$1
+        port=$3||80
+        puts "connecting to #{host} on #{port}..."
+        EM.connect(host, port, ProxyConnection, self, data)
+      end
+    end
+  end
+end
+
+EM.run {
+  EM.start_server("127.0.0.1", 8080, ProxyServer)
+}
